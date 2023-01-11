5e41f0370f655673888ee18a01199cb7da47343f
DESKTOP-BGB88JM\user <myatkaung2021@gmail.com>
2023-01-11 22:33:56 +0630

    Java Exercise 1

---
+++ TestIncrement.java
@@ -0,0 +1,20 @@
+package ClassExercise;
+
+public class TestIncrement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//ilnc with i++;
+		int i = 10; //declare i and assign value;
+		int ilnc = i++; //declare ilnc ;
+		
+		System.out.println("The result of ilnc is " + ilnc + ".");
+		System.out.println("The result of i is " + i + ".");
+//ilnc with ++i;	
+		ilnc = ++i; // declare ilnc and assign other value
+		
+		System.out.println("The result of ilnc is " + ilnc + ".");
+		System.out.println("The result of i is " + i + ".");
+	}
+
+}
